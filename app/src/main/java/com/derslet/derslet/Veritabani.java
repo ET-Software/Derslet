2f4aec944a6087b9aed822e815b1bddc7d57c598
Tarık Çınar <tarikcinar98@gmail.com>
2021-06-15 23:05:44 +0300

    Veritabanı Bağlantısını

---
+++ app/src/main/java/com/derslet/derslet/Veritabani.java
@@ -0,0 +1,61 @@
+package com.derslet.derslet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Veritabani {
+    private Connection connection;
+
+    // private final String host = "ssprojectinstance.csv2nbvvgbcb.us-east-2.rds.amazonaws.com"  // For Amazon Postgresql
+    private final String host = "35.198.78.89";  // For Google Cloud Postgresql
+    private final String database = "postgres";
+    private final int port = 5432;
+    private final String user = "postgres";
+    private final String pass = "erentarik0312";
+    private String url = "jdbc:postgresql://%s:%d/%s";
+    private boolean status;
+
+    public Veritabani() {
+        this.url = String.format(this.url, this.host, this.port, this.database);
+        connect();
+        //this.disconnect();
+        System.out.println("Baglanti Durumu:" + status);
+    }
+
+    private void connect() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Class.forName("org.postgresql.Driver");
+                    connection = DriverManager.getConnection(url, user, pass);
+                    status = true;
+                    System.out.println("Baglanti:" + status);
+                } catch (Exception e) {
+                    status = false;
+                    System.out.print(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.status = false;
+        }
+    }
+
+    public Connection getExtraConnection(){
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return c;
+    }
+}
