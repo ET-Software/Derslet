f296abad62dfec8141812b19174a9fc739518aff
Eren Erdoğan <dactarium@outlook.com>
2021-06-18 20:08:52 +0300

    Duyuru eklenmesi

--- app/src/main/java/com/derslet/derslet/OgretmenDuyuruEkle.java
+++ app/src/main/java/com/derslet/derslet/OgretmenDuyuruEkle.java
@@ -8,13 +8,18 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Timer;
@@ -25,6 +30,8 @@ public class OgretmenDuyuruEkle extends AppCompatActivity {
     ImageButton geri_buton;
     Button gönder_buton;
     AutoCompleteTextView ders_secim;
+    TextInputEditText duyuru_icerik;
+    int secim;
 
     Veritabani veritabani = new Veritabani();
     Statement stmt = null;
@@ -34,7 +41,9 @@ public class OgretmenDuyuruEkle extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ogretmen_duyuru_ekle);
 
+        duyuru_icerik = findViewById(R.id.ic_metin);
         ders_secim = findViewById(R.id.ic_liste); // Açılır ders filtresi
+        secim=-1;
 
         ArrayList<String> dersidleri = new ArrayList<String>(); // Veri tabanından çekilecek ders idleri
         ArrayList<String> dersisimleri = new ArrayList<String>(); // Veri tabanından çekilecek ders isimleri
@@ -67,7 +76,13 @@ public class OgretmenDuyuruEkle extends AppCompatActivity {
         //Ders isimlerinin açılır filtre listesine eklenmesi
         ArrayAdapter<String> ders_liste = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, dersisimleri);
+        ders_secim.setThreshold(1);
         ders_secim.setAdapter(ders_liste);
+        ders_secim.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                secim = arg2;
+            }
+        });
 
         //Butonlar
         geri_buton = (ImageButton)findViewById(R.id.geri_buton);
@@ -83,25 +98,45 @@ public class OgretmenDuyuruEkle extends AppCompatActivity {
         gönder_buton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String duyuruDurumu = "Duyuru gönderilirken hata meydana geldi!"; // databaseden gelicek yanıt göre belirlenecek toast mesajı
 
-                String duyuruDurumu = "Duyuru başarıyla eklendi!"; // databaseden gelicek yanıt göre belirlenecek toast mesajı
+                if(secim != -1){
+                    // Veritabanı Hata Giderici ('java.sql.Statement java.sql.Connection.createStatement()' on a null object reference)
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
 
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
+                    System.out.println("Secim: "+secim);
+                    System.out.println("Secim: "+dersidleri.get(secim));
+
+                    try {
+                        String sql = "INSERT INTO duyuru(dersid, icerik) VALUES ('" + dersidleri.get(secim) + "','" + duyuru_icerik.getText().toString() +"')";
+                        stmt.executeUpdate(sql);
+                        duyuruDurumu = "Duyuru başarıyla eklendi!";
+
+                        Timer timer = new Timer();
+                        timer.schedule(new TimerTask() {
                             @Override
                             public void run() {
-                                finish();
-                                OgretmenDuyuruEkle.this.overridePendingTransition(R.anim.fadein,R.anim.fadeout);
-                                timer.cancel();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        finish();
+                                        OgretmenDuyuruEkle.this.overridePendingTransition(R.anim.fadein,R.anim.fadeout);
+                                        timer.cancel();
+                                    }
+                                });
+
                             }
-                        });
+                        }, 1000); // 1sn bekliyor.
 
+                    } catch (Exception e){
+                        System.err.println("Gönderim hatası: " + e.getClass().getName() + ": " + e.getMessage());
+                        System.exit(0);
                     }
-                }, 1000); // 1sn bekliyor.
 
+                }else{
+                    duyuruDurumu = "Bir ders seçiniz!";
+                }
 
                 //Toast menü
                 Toast toast = Toast.makeText(getApplicationContext(), duyuruDurumu, Toast.LENGTH_SHORT);
